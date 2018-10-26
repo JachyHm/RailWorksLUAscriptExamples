333c51d24ab05f157b400e6817c3c9a6f792955b
Jáchym Hurtík <jachym.hurtik@gmail.com>
2018-10-27 01:51:05 +0200

    Repaired triggering target speed when passing restriction.
    Repaired triggering target speed when passing restriction.
    Now it plays sound only once.
    Still missing end of LZB procedure. Will be added soon.

--- LZB.lua
+++ LZB.lua
@@ -1,97 +1,151 @@
 LZB = { --objekt LZB
 	---------------LZB CONSTANTS---------------
+		DECCEL_U = 0.25,
+		DECCEL_M = 0.4,
+		DECCEL_O = 0.5,
 	---------------LZB VARIABLES---------------
 		active = false,
+		canBeActive = false,
 		maxSpeed = 0,
+		lastSpeedOfNextSignificantRestriction = 0,
+		lastDistanceToNextSignificantRestriction = 0,
 		distanceToNextSignificantRestriction = -1,
 		speedOfNextSignificantRestriction = -1,
+		targetSpeed = 200,
 		restrictionsCount = 0,
 		lastDistanceToRestriction = 0,
 		minimalDistanceToBrake = 0,
 		sourceSpeed = 0,
+		metersFromLastRestriction = 0,
+		blockedSpeedChangeCountdown = 0,
+		koefDecceleration = 1,
 	---------------LZB FUNCTIONS---------------
 		OnReceivedSignalMessage = function(self, message)
 			if message == "LZBSTART" then
-				LZB.active = true
+				LZB.canBeActive = true
 				-- SysCall("ScenarioManager:ShowAlertMessageExt", "LZB", "LZB is now active!", 2, 0)
-			elseif message == "LZBSTOP" then
-				LZB.active = false
+			elseif message == "LZBEND" then
+				LZB.canBeActive = false
 				-- SysCall("ScenarioManager:ShowAlertMessageExt", "LZB", "LZB is now deactive!", 2, 0)
 			end
 		end,
-		Update = function(self)
-			if LZB.active then
+		Update = function(self, deltaUpdateTimeFromGame)
+			if LZB.canBeActive and PZB90.on then
+				LZB.active = true
 				LZB.lastDistanceToRestriction = 0
 				typeOfRestriction = 100
 				LZB.restrictionsCount = 0
+				LZB.lastSpeedOfNextSignificantRestriction = LZB.speedOfNextSignificantRestriction
+				LZB.lastDistanceToNextSignificantRestriction = LZB.distanceToNextSignificantRestriction
+				if PZB90.trainType == PZB90.LOW then
+					LZB.koefDecceleration = LZB.DECCEL_U
+				elseif PZB90.trainType == PZB90.MIDDLE then
+					LZB.koefDecceleration = LZB.DECCEL_M
+				elseif PZB90.trainType == PZB90.HIGH then
+					LZB.koefDecceleration = LZB.DECCEL_O
+				end
 				--check for every speed restriction in next 10km and write them to array
 				while typeOfRestriction > 0 and LZB.lastDistanceToRestriction < 10000 do
-					typeOfRestriction, restrictedSpeed, distanceToRestriction = Call("GetNextSpeedLimit", 0, LZB.lastDistanceToRestriction, 10000)
+					if Call("GetControlValue", "Reverser", 0) > 0 then
+						typeOfRestriction, restrictedSpeed, distanceToRestriction = Call("GetNextSpeedLimit", 0, LZB.lastDistanceToRestriction, 10000)
+					elseif Call("GetControlValue", "Reverser", 0) < 0 then
+						typeOfRestriction, restrictedSpeed, distanceToRestriction = Call("GetNextSpeedLimit", 1, LZB.lastDistanceToRestriction, 10000)
+					else
+						typeOfRestriction = -1
+						restrictedSpeed = 200/3.6
+						distanceToRestriction = 10000
+					end
 					restrictedSpeed = restrictedSpeed * 3.6
-					if typeOfRestriction >= 0 and restrictedSpeed < LZB.sourceSpeed then
-						if LZB.restrictionsCount == 0 then
-							LZB.speedOfNextSignificantRestriction = restrictedSpeed
-							LZB.distanceToNextSignificantRestriction = distanceToRestriction
-						elseif LZB.distanceToNextSignificantRestriction == -1 or (distanceToRestriction-LZB.distanceToNextSignificantRestriction)+(restrictedSpeed-LZB.speedOfNextSignificantRestriction)*10 > 0 and restrictedSpeed < LZB.speedOfNextSignificantRestriction then
-							LZB.speedOfNextSignificantRestriction = restrictedSpeed
-							LZB.distanceToNextSignificantRestriction = distanceToRestriction
+					if typeOfRestriction >= 0 then
+						if restrictedSpeed < LZB.sourceSpeed and restrictedSpeed < 200 then
+							if LZB.restrictionsCount == 0 then
+								LZB.speedOfNextSignificantRestriction = restrictedSpeed
+								LZB.distanceToNextSignificantRestriction = distanceToRestriction
+							elseif (LZB.distanceToNextSignificantRestriction == -1 or (LZB.distanceToNextSignificantRestriction-distanceToRestriction)+(LZB.speedOfNextSignificantRestriction-restrictedSpeed)*10 > 0 and restrictedSpeed < LZB.speedOfNextSignificantRestriction) and restrictedSpeed < LZB.speedOfNextSignificantRestriction then
+								LZB.speedOfNextSignificantRestriction = restrictedSpeed
+								LZB.distanceToNextSignificantRestriction = distanceToRestriction
+							end
+							LZB.restrictionsCount = LZB.restrictionsCount + 1
 						end
-						LZB.restrictionsCount = LZB.restrictionsCount + 1
 					elseif LZB.restrictionsCount == 0 then
 						LZB.speedOfNextSignificantRestriction = 0
 						LZB.distanceToNextSignificantRestriction = -1
 					end
+					-- if Call("GetControlValue", "Befehl40", 0) > 0.5 then
+					-- 	Print("----------------")
+					-- 	Print(typeOfRestriction)
+					-- 	Print(restrictedSpeed)
+					-- 	Print(distanceToRestriction)
+					-- end
 					LZB.lastDistanceToRestriction = distanceToRestriction + 1
 				end
+				if LZB.speedOfNextSignificantRestriction - LZB.lastSpeedOfNextSignificantRestriction > 1 or (LZB.distanceToNextSignificantRestriction - LZB.lastDistanceToNextSignificantRestriction > 1) then
+					LZB.blockedSpeedChangeCountdown = 110
+				end
 				if LZB.speedOfNextSignificantRestriction < LZB.maxSpeed and LZB.distanceToNextSignificantRestriction ~= -1 then
-					LZB.minimalDistanceToBrake = LZB.sourceSpeed * (LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction) / 3.6 - (3.6 * ((LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction) / 3.6)^2) / 2
+					LZB.minimalDistanceToBrake = (LZB.sourceSpeed/3.6)*(LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction)/(3.6*LZB.koefDecceleration) - (LZB.koefDecceleration*((LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction)/(3.6*LZB.koefDecceleration))^2)/2
 				else
 					LZB.minimalDistanceToBrake = 0
 				end
-				local trackMax = Call("GetCurrentSpeedLimit") * 3.6
-				if LZB.minimalDistanceToBrake > math.max(0, LZB.distanceToNextSignificantRestriction-10) and LZB.distanceToNextSignificantRestriction ~= -1 then
-					LZB.maxSpeed = LZB.speedOfNextSignificantRestriction + ((LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction) * math.max(0, LZB.distanceToNextSignificantRestriction-10) / LZB.minimalDistanceToBrake)
-					if trackMax < LZB.maxSpeed then
-						Call("SetControlValue", "LZB_G_light", 0, 0)
-						LZB.maxSpeed = trackMax
+				if LZB.blockedSpeedChangeCountdown <= 0 then
+					local trackMax = Call("GetCurrentSpeedLimit") * 3.6
+					if LZB.minimalDistanceToBrake > math.max(0, LZB.distanceToNextSignificantRestriction-100) and LZB.distanceToNextSignificantRestriction ~= -1 then
+						LZB.maxSpeed = LZB.speedOfNextSignificantRestriction + ((LZB.sourceSpeed - LZB.speedOfNextSignificantRestriction) * math.max(0, LZB.distanceToNextSignificantRestriction-100) / LZB.minimalDistanceToBrake)
+						if trackMax < LZB.maxSpeed then
+							Call("SetControlValue", "LZB_G_light", 0, 0)
+							LZB.maxSpeed = math.min(trackMax, 200)
+							LZB.targetSpeed = math.min(trackMax, 200)
+						else
+							Call("SetControlValue", "LZB_G_light", 0, 1)
+							LZB.targetSpeed = LZB.speedOfNextSignificantRestriction
+						end
 					else
-						Call("SetControlValue", "LZB_G_light", 0, 1)
+						Call("SetControlValue", "LZB_G_light", 0, 0)
+						LZB.maxSpeed = math.min(trackMax, 200)
+						-- LZB.sourceSpeed = Call("GetSpeed") * 3.6
+						LZB.sourceSpeed = LZB.maxSpeed
+						LZB.targetSpeed = math.min(trackMax, 200)
 					end
+				-- if Call("GetControlValue", "Frei", 0) > 0.5 then
+				-- 	Print("----------------")
+				-- 	Print(LZB.distanceToNextSignificantRestriction)
+				-- 	Print(LZB.minimalDistanceToBrake)
+				-- 	Print(LZB.speedOfNextSignificantRestriction)
+				-- end
 				else
-					Call("SetControlValue", "LZB_G_light", 0, 0)
-					LZB.maxSpeed = trackMax
-					LZB.sourceSpeed = Call("GetSpeed") * 3.6
-				end
-				if Call("GetControlValue", "Befehl40", 0) > 0.5 then
-					Print("----------------")
-					Print(LZB.distanceToNextSignificantRestriction)
-					Print(LZB.minimalDistanceToBrake)
-					Print(LZB.speedOfNextSignificantRestriction)
+					LZB.blockedSpeedChangeCountdown = LZB.blockedSpeedChangeCountdown - (Call("GetSpeed")*deltaUpdateTimeFromGame)
 				end
 				if LZB.distanceToNextSignificantRestriction ~= -1 then
-					Call("SetControlValue", "LZB_DISTANCE_BAR", 0, math.min(1, LZB.distanceToNextSignificantRestriction/4000))
+					local typeOfRestriction, restrictedSpeed, distanceToRestriction = 0, 0, 0
+					if Call("GetControlValue", "Reverser", 0) > 0 then
+						typeOfRestriction, restrictedSpeed, distanceToRestriction = Call("GetNextSpeedLimit", 0, 0, 10000)
+					elseif Call("GetControlValue", "Reverser", 0) < 0 then
+						typeOfRestriction, restrictedSpeed, distanceToRestriction = Call("GetNextSpeedLimit", 1, 0, 10000)
+					else
+						typeOfRestriction = -1
+						restrictedSpeed = 0
+						distanceToRestriction = 0
+					end
+					Call("SetControlValue", "LZB_DISTANCE_BAR", 0, math.min(1, distanceToRestriction/4000))
 				else
 					Call("SetControlValue", "LZB_DISTANCE_BAR", 0, 1)
 				end
 
-				ARR:nastavRychlostLZB(LZB.maxSpeed, true)
-				Call("SetControlValue", "LZB_SPEED_DIAL", 0, math.min(1, LZB.maxSpeed/200))
+				while string.len(""..LZB.targetSpeed) < 3 do
+					LZB.targetSpeed = "0"..LZB.targetSpeed
+				end
+				Call("LZB_SPEED:SetText", LZB.targetSpeed, 0)
+
+				ARR:nastavRychlostLZB(math.max(LZB.maxSpeed-5, LZB.targetSpeed))
+				Call("SetControlValue", "LZB_SPEED_DIAL", 0, math.min(1, LZB.maxSpeed/220))
+				Call("SetControlValue", "PZB_ACTIVE", 0, 1)
 			else
 				Call("SetControlValue", "LZB_G_light", 0, 0)
 				Call("SetControlValue", "LZB_DISTANCE_BAR", 0, 0)
+				LZB.targetSpeed = 10000
+				Call("LZB_SPEED:SetText", "XXX", 0)
+				LZB.active = false
 			end
 			Call("SetControlValue", "LZB_ACTIVE", 0, ToBolAndBack(LZB.active))
 		end
 }
-function Update(deltaUpdateTimeFromGame)
-	deltaTime = math.abs(os.clock() - casMinuly)
-	casMinuly = os.clock()
-	if math.abs(deltaTime-deltaUpdateTimeFromGame) > 1 then
-		deltaTime = deltaUpdateTimeFromGame
-	end
-  --------------LZB-------------------
-		LZB:Update()
-end
-function OnCustomSignalMessage ( Parameter )
-	LZB:OnReceivedSignalMessage(Parameter)
-end
