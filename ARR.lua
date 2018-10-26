003a8ff1d317cc6c07bfe6e00ef844a48609dedd
Jáchym Hurtík <jachym.hurtik@gmail.com>
2018-10-27 01:58:27 +0200

    Completely reworked ARR
    ARR now works with acceleration, not with calculating "required" Effort.
    Must solve flying around required effort. Maybe by averaging.

--- ARR.lua
+++ ARR.lua
@@ -1,6 +1,8 @@
 ARR = { --objekt ARR
 	--PROMENNE:
 		cilovaRychlost = 0,
+		hodnotaTBC = 0,
+		rucneNabrzdeneTBC = 0,
 		LZBrychlost = 200,
 		LZBactive = false,
 		aktivni = false,
@@ -19,98 +21,228 @@ ARR = { --objekt ARR
 		deltaSpeedUpdateCount = 0,
 		deltaSpeedCount = 0,
 		avgDeltaSpeed = 0,
-
+		zrychleni = 0,
+		zrychleniMax = 0,
+		EDB = 0,
+		kladnyPT = 0,
+		snizujTlakPP = false,
+		zvysujTlakPP = true,
+		parkovacka = true,
+		blokujOdbrzdeniRucniVolba = false,
 	--METODY:
-		nastavCilovouRychlost = function(self,name,value)
+		nastavCilovouRychlost = function(self,name,value,add)
 			if value == 1 then
 				if KPJ:getIsActive() then
 					if string.find(name,"ARR_rych") ~= nil then
-						KPJ:wasClickedWhenCounting( 0+string.sub(name,9))
+						if add+string.sub(name,9) > ARR.cilovaRychlost then
+							KPJ:wasClickedWhenCounting(add+string.sub(name,9))
+						else
+							ARR.cilovaRychlost = add+string.sub(name,9)
+						end
 					end
 				else
 					if string.find(name,"ARR_rych") ~= nil then
-						ARR.cilovaRychlost = 0+string.sub(name,9)
+						ARR.cilovaRychlost = add+string.sub(name,9)
 					end
 				end
 			end
 		end,
-		nastavRychlostLZB = function(self,value,active)
+		nastavRychlostLZB = function(self,value)
 			ARR.LZBrychlost = value
 		end,
 		zapniARR = function(self)
 			ARR.cilovaRychlost = math.floor((Call("GetSpeed")*3.6)/5)*5+5
 			ARR.aktivni = true
-			ARR.tlak = 5
+			ARR.pm = 0.7
+			ARR.fm = 0.7
+			ARR.kladnyPT = 0
+			ARR.EDB = 0
 		end,
 		vypniARR = function(self)
-			ARR.cilovaRychlost = 0
+			ARR.cilovaRychlost = 200
 			ARR.aktivni = false
 			Call("SetControlValue","ARRRychlost",0,0)
+			ARR.kladnyPT = 0
+			ARR.EDB = 0
 		end,
-		mainStack = function(self, pridavek, deltaTime)
-			Call("SetControlValue","ARRRychlost",0,ARR.cilovaRychlost)
-			ARR.skutecnyPT = Call("GetControlValue","PomernyTah",0)
-			ARR.hmotnostVlaku = Call("GetConsistTotalMass")
-			ARR.delkaVlaku = Call("GetConsistLength")
-			ARR.stoupani = Call("GetGradient")
+		mainStack = function(self, pridavek, deltaTime, deltaUpdateTimeFromGame)
+			if ARR.aktivni then
+				Call("SetControlValue","ARRRychlost",0,ARR.cilovaRychlost)
+				ARR.skutecnyPT = Call("GetControlValue","PomernyTah",0)
+			else
+				Call("SetControlValue","ARRRychlost",0,0)
+				ARR.cilovaRychlost = 200
+				ARR.pm = 1
+				ARR.fm = 1
+				ARR.souhlas = true
+			end
+			-- ARR.hmotnostVlaku = Call("GetConsistTotalMass")
+			-- ARR.delkaVlaku = Call("GetConsistLength")
+			-- ARR.stoupani = Call("GetGradient")
 			local speed = math.abs(Call("GetSpeed")*3.6)
-			local deltaSpeed = (speed-ARR.rychlostKMH)/deltaTime
-			if self.deltaSpeedUpdateCount < 10 then
-				self.deltaSpeedUpdateCount = self.deltaSpeedUpdateCount + 1
-				self.deltaSpeedCount = self.deltaSpeedCount + deltaSpeed
-			else
-				self.deltaSpeedUpdateCount = 0
-				self.avgDeltaSpeed = self.deltaSpeedCount/10
-				self.deltaSpeedCount = 0
-			end
+			-- local deltaSpeed = (speed-ARR.rychlostKMH)/deltaUpdateTimeFromGame*3.6
+			-- if self.deltaSpeedUpdateCount < 10 then
+			-- 	self.deltaSpeedUpdateCount = self.deltaSpeedUpdateCount + 1
+			-- 	self.deltaSpeedCount = self.deltaSpeedCount + deltaSpeed
+			-- else
+			-- 	self.deltaSpeedUpdateCount = 0
+			-- 	ARR.zrychleni = self.deltaSpeedCount/10
+			-- 	self.deltaSpeedCount = 0
+			-- end
 			ARR.rychlostKMH = speed
-			local cilovyPT = 0
-			local rozdilovaSlozka = (math.min(ARR.cilovaRychlost, ARR.LZBrychlost)-ARR.rychlostKMH)/10
-			local koefZrychleni = math.min(math.abs(2/(ARR.avgDeltaSpeed)*rozdilovaSlozka),1)
-			local gradientSlozka = 0
-			local koefEDB = 500
-			local koefHmotnost = 350 --550
-			local hmotnostniSlozka = (ARR.hmotnostVlaku-84)/koefHmotnost
-			if Call("GetControlValue", "CisloKab", 0) == 1 or (Call("GetControlValue", "CisloKab", 0) == 0 and Call("GetControlValue", "Reverser", 0) == -1) then
-				gradientSlozka = ARR.stoupani / 100
+			ARR.zrychleniMax = (math.min(ARR.cilovaRychlost, ARR.LZBrychlost) - ARR.rychlostKMH)/15
+			
+			if (Call("GetControlValue", "CisloKab", 0) == 1 and Call("GetControlValue", "Reverser", 0) == 1) or (Call("GetControlValue", "CisloKab", 0) == 0 and Call("GetControlValue", "Reverser", 0) == -1) then
+				ARR.zrychleni = -Call("GetAcceleration")
 			else
-				gradientSlozka = -ARR.stoupani / 100
+				ARR.zrychleni = Call("GetAcceleration")
 			end
-			if math.min(ARR.cilovaRychlost, ARR.LZBrychlost) > ARR.rychlostKMH and ARR.souhlas and pridavek == 0 then
-				cilovyPT = math.min((rozdilovaSlozka+math.max(((hmotnostniSlozka*gradientSlozka*koefEDB)*rozdilovaSlozka),0))*koefZrychleni,1)
-				ARR.tlak = 5
-			elseif math.min(ARR.cilovaRychlost, ARR.LZBrychlost) < ARR.rychlostKMH then
-				cilovyPT = math.min(math.max((rozdilovaSlozka+math.min(((hmotnostniSlozka*(-gradientSlozka)*koefEDB)*rozdilovaSlozka),0))*koefZrychleni,-1),pridavek)
-				if (ARR.skutecnyPT < -0.9 and ARR.avgDeltaSpeed > (-(((-rozdilovaSlozka*10+0.33)^(1/2.5))/1))+0.64) or ARR.tlak ~= 5 then
-					if ARR.avgDeltaSpeed > (-(((-rozdilovaSlozka*10+0.33)^(1/2.5))/1))+0.64 then
-						ARR.tlak = ARR.tlak - 0.01
-					elseif ARR.avgDeltaSpeed-(-(((-rozdilovaSlozka*10+0.33)^(1/2.5))/1))+0.64 > 0.1 then
-						ARR.tlak = ARR.tlak + 0.01
-						if ARR.tlak > 5 then
-							ARR.tlak = 5
+
+			if ARR.souhlas and pridavek == 0 then
+				if ARR.zrychleni < ARR.zrychleniMax and ((ARR.rychlostKMH < 30 and ARR.kladnyPT < math.min(ARR.pm, ARR.fm)) or (ARR.rychlostKMH > 30 and ARR.kladnyPT < ARR.pm)) then
+					if ARR.EDB == 0 and ARR.kladnyPT < 1 then
+						if ARR.rychlostKMH < 30 then
+							ARR.kladnyPT = math.min(ARR.kladnyPT + ARR.koeficientPohybuPT * deltaTime, 1, ARR.pm, ARR.fm)
+						else
+							ARR.kladnyPT = math.min(ARR.kladnyPT + ARR.koeficientPohybuPT * deltaTime, 1, ARR.pm)
+						end
+					elseif ARR.EDB ~= 0 then
+						ARR.EDB = math.min(ARR.EDB + ARR.koeficientPohybuPT * deltaTime, 0)
+					end
+					ARR.snizujTlakPP = false
+					ARR.zvysujTlakPP = true
+				elseif ARR.zrychleni > ARR.zrychleniMax or ((ARR.rychlostKMH < 30 and ARR.kladnyPT > math.min(ARR.pm, ARR.fm)) or (ARR.rychlostKMH > 30 and ARR.kladnyPT > ARR.pm)) then
+					if ARR.kladnyPT == 0 and ARR.EDB > -1 then
+						ARR.EDB = math.max(ARR.EDB - ARR.koeficientPohybuPT * deltaTime, -1)
+					elseif ARR.kladnyPT ~= 0 then
+						ARR.kladnyPT = math.max(ARR.kladnyPT - ARR.koeficientPohybuPT * deltaTime, 0)
+					end
+					if ARR.EDB == -1 and ARR.zrychleni > -0.7 then
+						ARR.snizujTlakPP = true
+						ARR.zvysujTlakPP = false
+					elseif ARR.rychlostKMH - math.min(ARR.cilovaRychlost, ARR.LZBrychlost) > 20 then
+						ARR.snizujTlakPP = true
+						ARR.zvysujTlakPP = false
+					else
+						ARR.snizujTlakPP = false
+					end
+				end
+			else
+				if ARR.zrychleni > ARR.zrychleniMax then
+					if ARR.kladnyPT == 0 and ARR.EDB > -1 then
+						ARR.EDB = math.max(ARR.EDB - ARR.koeficientPohybuPT * deltaTime, -1)
+					elseif ARR.kladnyPT ~= 0 then
+						ARR.kladnyPT = math.max(ARR.kladnyPT - ARR.koeficientPohybuPT * deltaTime, 0)
+					end
+					if (ARR.EDB == -1 and ARR.zrychleni > -0.7) or (ARR.rychlostKMH - math.min(ARR.cilovaRychlost, ARR.LZBrychlost) > 20) then
+						ARR.snizujTlakPP = true
+						ARR.zvysujTlakPP = false
+					else
+						ARR.snizujTlakPP = false
+					end
+				else
+					ARR.snizujTlakPP = false
+					ARR.zvysujTlakPP = true
+					if ARR.kladnyPT ~= 0 then
+						ARR.kladnyPT = math.max(ARR.kladnyPT - ARR.koeficientPohybuPT * deltaTime, 0)
+					end
+					if ARR.EDB ~= 0 and pridavek == 0 then
+						ARR.EDB = math.min(ARR.EDB + ARR.koeficientPohybuPT * deltaTime, 0)
+					elseif pridavek < 0 then
+						if pridavek - ARR.EDB > 0.01 then
+							ARR.EDB = math.min(ARR.EDB + ARR.koeficientPohybuPT * deltaTime, 0)
+						elseif pridavek - ARR.EDB < -0.01 then
+							ARR.EDB = math.max(ARR.EDB - ARR.koeficientPohybuPT * deltaTime, -1)
+						else
+							ARR.EDB = math.min(math.max(pridavek, -1), 1)
 						end
 					end
 				end
 			end
-			if pridavek ~= 0 and cilovyPT <= 0 then
-				cilovyPT = math.min(cilovyPT,pridavek)
+
+			if not ARR.parkovacka then
+				if ARR.EDB < 0 then
+					ARR.skutecnyPT = ARR.EDB
+				elseif ARR.kladnyPT > 0 then
+					ARR.skutecnyPT = ARR.kladnyPT
+				else
+					ARR.skutecnyPT = 0
+				end
+			else
+				if ARR.EDB < 0 then
+					ARR.EDB = math.min(ARR.EDB + ARR.koeficientPohybuPT * deltaTime, 0)
+				elseif ARR.kladnyPT > 0 then
+					ARR.skutecnyPT = ARR.kladnyPT
+				else
+					ARR.skutecnyPT = 0
+				end
 			end
-			if ARR.rychlostKMH < 30 then
-				cilovyPT = math.max(math.min(cilovyPT, ARR.fm), -ARR.fm)
-			end
-			cilovyPT = math.max(math.min(cilovyPT, ARR.pm), -ARR.pm)
+
 			if ARR.rychlostKMH <= 3 then
 				ARR.souhlas = false
 			end
-			if math.abs(cilovyPT - ARR.skutecnyPT) > ARR.koeficientPohybuPT*deltaTime*2 then
-				if cilovyPT > ARR.skutecnyPT then
-					ARR.skutecnyPT = ARR.skutecnyPT + ARR.koeficientPohybuPT*deltaTime
-				else
-					ARR.skutecnyPT = ARR.skutecnyPT - ARR.koeficientPohybuPT*deltaTime
-				end
-			else
-				ARR.skutecnyPT = cilovyPT
-			end
+			-- local rozdilovaSlozka = (math.min(ARR.cilovaRychlost, ARR.LZBrychlost)-ARR.rychlostKMH)/10
+			-- if rozdilovaSlozka > 0 then
+			-- 	rozdilovaSlozka = rozdilovaSlozka*math.max(ARR.rychlostKMH/100, 1)
+			-- end
+			-- local koefZrychleni = math.min(math.abs(4/ARR.avgDeltaSpeed), 1)
+			-- local gradientSlozka = 0
+			-- local koefKladnyPT = 1
+			-- local koefZapornyPT = 4
+			-- local koefHmotnost = 550
+			-- local hmotnostniSlozka = 0
+			-- if Call("GetControlValue", "CisloKab", 0) == 1 or (Call("GetControlValue", "CisloKab", 0) == 0 and Call("GetControlValue", "Reverser", 0) == -1) then
+			-- 	gradientSlozka = math.max(ARR.stoupani+1, 1) --/10
+			-- else
+			-- 	gradientSlozka = math.max(-ARR.stoupani+1, 1)--/10
+			-- end
+			-- if math.min(ARR.cilovaRychlost, ARR.LZBrychlost) > ARR.rychlostKMH and ARR.souhlas and pridavek == 0 and LZB.targetSpeed > ARR.rychlostKMH then
+			-- 	if gradientSlozka >= 0 then
+			-- 		hmotnostniSlozka = math.max((ARR.hmotnostVlaku+466)/koefHmotnost, 1)
+			-- 	else
+			-- 		hmotnostniSlozka = math.max(koefHmotnost/(ARR.hmotnostVlaku+466), 0)
+			-- 	end
+			-- 	ARR.cilovyPT = math.min((rozdilovaSlozka+math.max(((hmotnostniSlozka*gradientSlozka*koefKladnyPT)*rozdilovaSlozka),0))*koefZrychleni,1)
+			-- 	ARR.hodnotaTBC = 5
+			-- elseif math.min(ARR.cilovaRychlost, ARR.LZBrychlost) < ARR.rychlostKMH then
+			-- 	if gradientSlozka >= 0 then
+			-- 		hmotnostniSlozka = math.max(koefHmotnost/(ARR.hmotnostVlaku+466), 0)
+			-- 	else
+			-- 		hmotnostniSlozka = math.max((ARR.hmotnostVlaku+466)/koefHmotnost, 1)
+			-- 	end
+			-- 	rozdilovaSlozka = rozdilovaSlozka*4
+			-- 	ARR.cilovyPT = math.min((rozdilovaSlozka+math.min(((hmotnostniSlozka*(-gradientSlozka)*koefZapornyPT)*rozdilovaSlozka),0))*koefZrychleni,pridavek)
+			-- 	if ARR.cilovyPT < -10 or ARR.hodnotaTBC ~= 5 then
+			-- 		ARR.hodnotaTBC = math.min(5-(ARR.cilovyPT + 10)/10,5)
+			-- 	end
+			-- 	ARR.cilovyPT = math.max(ARR.cilovyPT,-1)
+			-- end
+			-- if pridavek ~= 0 then
+			-- 	ARR.cilovyPT = math.min(ARR.cilovyPT,pridavek)
+			-- end
+			-- if cilovyPT >= 0 then
+			-- 	if ARR.rychlostKMH < 30 then
+			-- 		cilovyPT = math.max(math.min(cilovyPT, ARR.fm), -ARR.fm)
+			-- 	end
+			-- 	ARR.cilovyPT = math.max(math.min(ARR.cilovyPT, ARR.pm), -ARR.pm)
+			-- end
+			-- Call("SetControlValue", "ARR_DEBUG_DIFFERENCIAL", 0, rozdilovaSlozka)
+			-- Call("SetControlValue", "ARR_DEBUG_ACCELERATION", 0, koefZrychleni)
+			-- Call("SetControlValue", "ARR_DEBUG_GRADIENT", 0, gradientSlozka)
+			-- Call("SetControlValue", "ARR_DEBUG_MASS", 0, hmotnostniSlozka)
+			-- Call("SetControlValue", "ARR_DEBUG_PT", 0, ARR.cilovyPT)
+			-- if ARR.rychlostKMH <= 3 then
+			-- 	ARR.souhlas = false
+			-- end
+			-- if math.abs(ARR.cilovyPT - ARR.skutecnyPT) > ARR.koeficientPohybuPT*deltaTime*2 then
+			-- 	if ARR.cilovyPT > ARR.skutecnyPT then
+			-- 		ARR.skutecnyPT = ARR.skutecnyPT + ARR.koeficientPohybuPT*deltaTime
+			-- 	else
+			-- 		ARR.skutecnyPT = ARR.skutecnyPT - ARR.koeficientPohybuPT*deltaTime
+			-- 	end
+			-- else
+			-- 	ARR.skutecnyPT = ARR.cilovyPT
+			-- end
 			return(ARR.skutecnyPT)
 		end,
 		udelSouhlas = function(self)
@@ -118,6 +250,9 @@ ARR = { --objekt ARR
 		end,
 		vybeh = function(self)
 			ARR.souhlas = false
+		end,
+		nastavParkovacku = function(self, hodnota)
+			ARR.parkovacka = hodnota
 		end,
 		pmPlus = function(self)
 			ARR.pm = ARR.pm + 0.1
@@ -157,33 +292,41 @@ ARR = { --objekt ARR
 		getFM = function(self)
 			return(ARR.fm)
 		end,
-		getRequiredPress = function(self)
-			return(ARR.tlak or 5)
+		rychPlus = function(self)
+			ARR.cilovaRychlost = math.min(ARR.cilovaRychlost + 5, 200)
+		end,
+		rychMinus = function(self)
+			ARR.cilovaRychlost = math.max(ARR.cilovaRychlost - 5, 0)
+		end,
+		getTBCvalue = function(self)
+			return(ARR.hodnotaTBC or 0)
 		end
 };
 KPJ = { --objekt KPJ
 	remainingMeters = 0,
 	isActive = false,
 	afterTurnOFFCounter = 0,
-	afterTurnOFFSpeed = 0,
+	afterTurnOFFSpeed = -1,
 
 	startCounting = function(self, metersToGo)
 		KPJ.remainingMeters = metersToGo
 		KPJ.isActive = true
-		KPJ.afterTurnOFFSpeed = ARR.cilovaRychlost
+		KPJ.afterTurnOFFSpeed = -1
 	end,
 
 	update = function(self, casUpdateGame, casUpdate)
 		if KPJ.isActive then
 			Call("SetControlValue", "KPJzobraz", 0, 1)
-			local speed = Call("GetSpeed") or 0
+			local speed = math.abs(Call("GetSpeed")) or 0
 			local metersFromLastUpdate = speed * casUpdateGame
 			KPJ.remainingMeters = KPJ.remainingMeters - metersFromLastUpdate
 			if KPJ.remainingMeters <= 0 then
 				KPJ.isActive = false
 				KPJ.remainingMeters = 0
 				KPJ.afterTurnOFFCounter = 2
-				ARR.cilovaRychlost = KPJ.afterTurnOFFSpeed
+				if KPJ.afterTurnOFFSpeed ~= -1 and ARR.cilovaRychlost < KPJ.afterTurnOFFSpeed then
+					ARR.cilovaRychlost = KPJ.afterTurnOFFSpeed
+				end
 			end
 			return(KPJ.remainingMeters)
 		end
